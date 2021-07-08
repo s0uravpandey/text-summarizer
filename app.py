2841c3272367fd7140ce8e7e86a058d890575f5c
s0uravpandey <59795737+s0uravpandey@users.noreply.github.com>
2021-07-08 19:24:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def predict():
     for word in doc:
         if word.text.lower() not in stopwords:
             if word.text.lower() not in punctuation:
-                if word.text.lower() not in word_frequencies.keys():
+                if word.text not in word_frequencies.keys():
                     word_frequencies[word.text]=1
                 else:
                         word_frequencies[word.text]+=1
